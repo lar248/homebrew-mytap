e37db2a8d7a216438ef2030d6d58d68d44163c95
Laurence <laurence.rosenzweig@capitalone.com>
2015-07-28 10:57:23 -0400

    added file

---
+++ formalfridayclub.rb
@@ -0,0 +1,11 @@
+class Formalfridayclub < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/lar248/formalfridayclub/archive/1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "67a2116586d93a1a55a5c76290d68020675e6bebd61b92a444f184585398c99d"
+
+  def install
+    bin.install "formalfriday"
+  end
+end
